912e8658c71ff783ff129ff3200dba3af7f5a42b
Mark <mark@bhalash.com>
2014-07-10 05:57:23 +0100

    Added smartphone test
    Menu no longer animates opening and closing on mobile devices, for the
    sake of performance.

--- anchor/functions.js
+++ anchor/functions.js
@@ -1,39 +1,51 @@
+var isSmartphone = false;
+var menuIsOpen = false;
+
+function testSmartphone() {
+    // Test if client is a smartphone. UA sniffing is iffy.
+    var mob = 'mobile-device-test';
+    $('body').append('<div class="' + mob + '">');
+    isSmartphone = ($('.' + mob).css('display') === 'none') ? true : false;
+    $('body').remove('.' + mob);
+}
+
+function openMenu() {
+    // Same code in closeMenu(). I don't want to animate menu on phones.
+    // It looks juddery, depending on the handset. 
+    if (!isSmartphone) {
+        $('nav .menu-wrap').slideDown();
+    } else {
+        $('nav .menu-wrap').toggle();
+    }
+
+    $('nav .button').css('background-color', '#ba3434');
+    menuIsOpen = true;
+}
+
+function closeMenu() {
+    if (!isSmartphone) {
+        $('nav .menu-wrap').slideUp();
+    } else {
+        $('nav .menu-wrap').toggle();
+    }
+
+    $('nav .button').css('background-color', '#343537');
+    menuIsOpen = false;
+}
+
 $(function() {
     // For google-code-prettify
     $('pre').each(function() {
         $(this).addClass('prettyprint');
     });    
 
-    // Get a better solution, Mark.
-    $('.content-wrap').css('min-height', $(window).height() + 'px');
+    testSmartphone();
 });
-
-var menuIsOpen = false;
-
-function openMenu() {
-    $('nav .menu-wrap').slideDown();
-    $('nav .button').css('background-color', '#ba3434');
-    menuIsOpen = true;
-}
-
-function closeMenu() {
-    $('nav .menu-wrap').slideUp();
-    $('nav .button').css('background-color', '#343537');
-    menuIsOpen = false;
-}
 
 $('nav .button').click(function() {
     if (!menuIsOpen) {
         openMenu();
     } else {
         closeMenu();
-    }
-});
-
-$('body').keyup(function(k) {
-    if (k.keyCode == 27) {
-        if (menuIsOpen) {
-            closeMenu();
-        }
     }
 });
