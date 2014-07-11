fea72b6c91a0eea4689ca7349b0cd2f0bf27f545
Mark <mark@bhalash.com>
2014-07-11 19:17:55 +0100

    Moved pagination to footer

--- makeout_point/footer.php
+++ makeout_point/footer.php
@@ -1,5 +1,14 @@
+    <footer>
+        <?php if(has_pagination()): ?>
+            <p class="pagination">
+                <!-- TODO: FIXME -->
+                <span class="previous"><a href="<?php echo posts_prev(); ?>">Previous Page</a></span>
+                <span class="next"><a href="<?php echo posts_next(); ?>">Next Page</a></span>
+            </p>
+        <?php endif; ?>
+    </footer>
     </div>
-    <script src="<?php echo theme_url('jquery-2.1.0.min.js'); ?>"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="<?php echo theme_url('prettify/prettify.js'); ?>"></script>
     <script src="<?php echo theme_url('functions.js'); ?>"></script>
 </body>
