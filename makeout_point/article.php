bdda7cf90030d0e2a807bf4a4aa49e51cb2833fe
Mark <mark@bhalash.com>
2014-07-11 23:01:17 +0100

    Added post commentary
    Style will be changed again-looks awful.

--- makeout_point/article.php
+++ makeout_point/article.php
@@ -1,41 +1,28 @@
 <?php theme_include('header'); ?>
     <article id="article-<?php echo article_id(); ?>">
-        <h3 class="post-title"><a href="<?php echo article_url(); ?>"><?php echo article_title();?></a></h3>
-        <p class="post-info">by <a href="mailto:<?php echo user_authed_email(); ?>"><?php echo article_author(); ?></a> on <time datetime="<?php echo article_date(); ?>"><?php echo article_date(); ?></time></p>
+        <h3 class="article-title"><a href="<?php echo article_url(); ?>"><?php echo article_title();?></a></h3>
+        <p class="article-info">by <a href="mailto:<?php echo user_authed_email(); ?>"><?php echo article_author(); ?></a> on <time datetime="<?php echo article_date(); ?>"><?php echo article_date(); ?></time></p>
         <?php echo article_markdown(); ?>
     </article>
     <?php if(comments_open()): ?>
     <section class="comments">
         <?php if(has_comments()): ?>
             <ul class="commentlist">
-                <?php $i = 0; while(comments()): $i++; ?>
+                <?php while(comments()): ?>
                     <li class="comment" id="comment-<?php echo comment_id(); ?>">
-                        <div class="wrap">
-                            <span class="counter"><?php echo $i; ?></span>
-                            <h5><?php echo comment_name(); ?></h5>
-                            <div class="content">
-                                <?php echo comment_text(); ?>
-                            </div>
-                        </div>
+                        <h6><?php echo comment_name(); ?></h6>
+                        <span class="comment-date"><time datetime="<?php echo comment_date(); ?>"><?php echo comment_date(); ?></time></span><br />
+                        <?php echo comment_text(); ?>
                     </li>
                 <?php endwhile; ?>
             </ul>
         <?php endif; ?>
-        <form id="comment" class="commentform wrap" method="post" action="<?php echo comment_form_url(); ?>#comment">
-            <?php echo comment_form_notifications(); ?>
-            <p class="name">
-                <label for="name">Your name:</label>
+        <form id="comment" class="commentform" method="post" action="<?php echo comment_form_url(); ?>#comment">
+            <p>
+                <?php echo comment_form_notifications(); ?>
                 <?php echo comment_form_input_name('placeholder="Your name"'); ?>
-            </p>
-            <p class="email">
-                <label for="email">Your email address:</label>
                 <?php echo comment_form_input_email('placeholder="Your email (won’t be published)"'); ?>
-            </p>
-            <p class="textarea">
-                <label for="text">Your comment:</label>
                 <?php echo comment_form_input_text('placeholder="Your comment"'); ?>
-            </p>
-            <p class="submit">
                 <?php echo comment_form_button(); ?>
             </p>
         </form>
