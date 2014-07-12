3166c076b5acb7fa31ff5e8bc3883d9a6396486d
Mark <mark@bhalash.com>
2014-07-12 12:45:48 +0100

    Completed 404 and search templates
    Site is now functionally complete.

---
+++ makeout_point/404.php
@@ -0,0 +1,6 @@
+<?php theme_include('header'); ?>
+    <div class="fail">
+        <h4>404: Page not found</h4>
+        <p>The eyes of my ancestors are cast down in shame!</p>
+    </div>
+<?php theme_include('footer'); ?>
--- makeout_point/search.php
+++ makeout_point/search.php
@@ -1,6 +1,13 @@
 <?php theme_include('header'); ?>
-<p>Results for &ldquo;<?php echo search_term(); ?>&rdquo;.</p>
 <?php if(has_search_results()) : while(search_results()) : ?>
+    <div class="fail">
+        <h4>Results for '<?php echo search_term(); ?>'</h4>
+        <?php if(total_search_results() == 1) : ?>
+            <p><?php echo total_search_results(); ?> total result.</p>
+        <?php else: ?>
+            <p><?php echo total_search_results(); ?> total results.</p>
+        <?php endif; ?>
+    </div>
     <article>
         <h3 class="article-title"><a href="<?php echo article_url(); ?>"><?php echo article_title();?></a></h3>
         <p class="article-info">by <a href="mailto:<?php echo user_authed_email(); ?>"><?php echo article_author(); ?></a> on <time datetime="<?php echo article_date(); ?>"><?php echo article_date(); ?></time></p>
@@ -14,8 +21,11 @@
         </div>
     </nav>
     <?php endif; ?>
-<?php endwhile; endif; ?>
+<?php endwhile; ?>
 <?php else: ?>
-    <p class="wrap">No match for &ldquo;<?php echo search_term(); ?>&rdquo;. Fail.</p>
+    <div class="fail">
+        <h4>No match for '<?php echo search_term(); ?>'</h4>
+        <p>The eyes of my ancestors are cast down in shame!</p>
+    </div>
 <?php endif; ?>
 <?php theme_include('footer'); ?>
