49cd53e0a611385125258a041364fd11b386ab3d
Mark Grealish <mark@bhalash.com>
2015-11-08 15:50:20 +0000

    Removed external CSS libraries from loader

--- footer.php
+++ footer.php
@@ -1,6 +1,4 @@
     </section>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/prettify/r224/prettify.js" type="text/javascript"></script>
     <script src="<?php echo theme_url('js/functions.js'); ?>"></script>
 </body>
 </html>
