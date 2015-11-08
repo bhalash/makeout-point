314c6953b3f8e5052e7380b37235d3f167e0150f
Mark Grealish <mark@bhalash.com>
2015-11-08 15:57:51 +0000

    Wrapped theme functions in self-contained function

--- assets/js/makeout.js
+++ assets/js/makeout.js
@@ -1,72 +1,52 @@
-var isSmartphone = false;
-var menuIsOpen = false;
+(function(window, document) {
+    var menuIsOpen = false;
 
-function testSmartphone() {
-    // Test if client is a smartphone. UA sniffing is iffy.
-    var mob = 'mobile-device-test';
-    $('body').append('<div class="' + mob + '">');
-    isSmartphone = ($('.' + mob).css('display') === 'none') ? true : false;
-    $('body').remove('.' + mob);
-}
+    function openMenu(button) {
+        // Same code in closeMenu(). I don't want to animate menu on phones.
+        // It looks juddery, depending on the handset. 
+        var wrap = (button.hasClass('menu-button')) ? '.menu-wrap' : '.comments-wrap';
 
-function openMenu(button) {
-    // Same code in closeMenu(). I don't want to animate menu on phones.
-    // It looks juddery, depending on the handset. 
-    var wrap = (button.hasClass('menu-button')) ? '.menu-wrap' : '.comments-wrap';
+        if (!isSmartphone) {
+            $(wrap).slideDown();
 
-    if (!isSmartphone) {
-        $(wrap).slideDown();
+            if ($(button).hasClass('comments-button'))
+                $('body,html').animate({'scrollTop' : $(document).height()}, 400);
+        } else {
+            $(wrap).toggle();
+        }
 
-        if ($(button).hasClass('comments-button'))
-            $('body,html').animate({'scrollTop' : $(document).height()}, 400);
-    } else {
-        $(wrap).toggle();
+        if (button.hasClass('button'))
+            $(button).css('background-color', '#ba3434');
+
+        menuIsOpen = true;
     }
 
-    if (button.hasClass('button'))
-        $(button).css('background-color', '#ba3434');
+    function closeMenu(button) {
+        var wrap = (button.hasClass('menu-button')) ? '.menu-wrap' : '.comments-wrap';
 
-    menuIsOpen = true;
-}
+        if (!isSmartphone) {
+            $(wrap).slideUp();
+        } else {
+            $(wrap).toggle();
+        }
 
-function closeMenu(button) {
-    var wrap = (button.hasClass('menu-button')) ? '.menu-wrap' : '.comments-wrap';
-
-    if (!isSmartphone) {
-        $(wrap).slideUp();
-    } else {
-        $(wrap).toggle();
+        if (button.hasClass('button'))
+            $(button).css('background-color', '#343537');
+        menuIsOpen = false;
     }
 
-    if (button.hasClass('button'))
-        $(button).css('background-color', '#343537');
-    menuIsOpen = false;
-}
-
-function initialMenuOpen() {
-    $('.comments-wrap').css('display', 'block');
-    $('body,html').animate({'scrollTop' : $(document).height()}, 400);
-    $('.comments .comments-button').css('background-color', '#ba3434');
-    menuIsOpen = true;
-}
-
-$(function() {
-    // For google-code-prettify
-    $('pre').each(function() {
-        $(this).addClass('prettyprint');
-    });    
-    
-    testSmartphone();
-
-    // Opens the page at the comments if the comments link was followed.
-    if (document.URL.indexOf('#comments') >= 0)
-        initialMenuOpen();
-});
-
-$('.comments-button, .menu-button').click(function() {
-    if (!menuIsOpen) {
-        openMenu($(this));
-    } else {
-        closeMenu($(this));
+    function initialMenuOpen() {
+        $('.comments-wrap').css('display', 'block');
+        $('body,html').animate({'scrollTop' : $(document).height()}, 400);
+        $('.comments .comments-button').css('background-color', '#ba3434');
+        menuIsOpen = true;
     }
-});
+
+    $('.comments-button, .menu-button').click(function() {
+        if (!menuIsOpen) {
+            openMenu($(this));
+        } else {
+            closeMenu($(this));
+        }
+    });
+})(window, document);
