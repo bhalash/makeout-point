2e9292c3e669a535585bd5ab552be61b05784a72
Mark Grealish <mark@bhalash.com>
2015-11-29 17:07:43 +0000

    Refactored header CSS selectors into BEM style.

--- header.php
+++ header.php
@@ -15,49 +15,52 @@
     <meta property="og:description" content="<?php echo site_description(); ?>">
 </head>
 <body>
-    <header class="head" id="head">
-        <div class="banner">
-            <div class="title">
-                <h4><a href="<?php echo base_url(); ?>"><?php echo site_name(); ?></a></h4>
+    <div class="makeout" id="makeout">
+        <header class="header makeout__header" id="head">
+            <div class="header__banner">
+                <h4 class="header__title"><a class="header__site-link" href="<?php echo base_url(); ?>"><?php echo site_name(); ?></a></h4>
             </div>
-        </div>
-        <div class="logo-wrap">
-            <!-- Set this in functions.php -->
-            <div class="logo"><a href="<?php echo twitter_url(); ?>"></a></div>
-        </div>
-    </header>
-    <main class="content">
-        <nav>
-            <div class="button menu-button"><a href="javascript:void(0)"></a></div>
-            <div class="wrap menu-wrap">
-                <div class="menu">
-                    <form id="search" action="<?php echo search_url(); ?>" method="post">
-                        <input id="term" name="term" placeholder="Type and then hit enter to search" tabindex="1" type="search" value="<?php echo search_term(); ?>">
-                    </form>
-                    <?php if (has_menu_items()) : ?> 
-                        <ul class="left">
-                            <?php while(menu_items()): ?> 
-                                <li <?php echo (menu_active() ? 'class="active"' : ''); ?>>
-                                    <a class="list-alt" href="<?php echo menu_url(); ?>" title="<?php echo menu_title(); ?>"><?php echo menu_name(); ?></a>
-                                </li>
-                            <?php endwhile; ?>
-                        </ul>
-                        <ul>
-                            <?php while(categories()) : ?>
-                                <li>
-                                    <a href="<?php echo category_url(); ?>" title="<?php echo category_description(); ?>">
-                                        <?php echo category_title(); ?> (<?php echo category_count(); ?>)
-                                    </a>
-                                </li>
-                            <?php endwhile; ?>
-                        </ul>
-                        <ul class="right">
-                            <li><a class="beard" href="http://www.bhalash.com" title="Real Men Wear Beards">Real Men Wear Beards</a></li>
-                            <li><a class="facebook" href="http://www.facebook.com" title="Facebook">Facebook</a></li>
-                            <li><a class="labs" href="http://www.091labs.com" title="091 Labs">091 Labs</a></li>
-                            <li><a class="rss" href="<?php rss_url(); ?>" title="RSS Feed">RSS Feed</a></li>
-                        </ul>
-                    <?php endif; ?>
+            <div class="header__logo">
+                <!-- Set this in functions.php -->
+                <div class="header__logo-wrap">
+                    <a class="header__logo-link" href="<?php echo twitter_url(); ?>"></a>
                 </div>
             </div>
-        </nav>
+        </header>
+        <main class="makeout__content">
+            <nav class="menu">
+                <div class="button menu__button"><a href="javascript:void(0)"></a></div>
+                <div class="menu__wrap">
+                    <div class="menu__content">
+                        <form class="menu__search form searchform" id="search" action="<?php echo search_url(); ?>" method="post">
+                            <fieldset>
+                                <input class="menu__input searchform__input" id="term" name="term" placeholder="Type and then hit enter to search" tabindex="1" type="search" value="<?php echo search_term(); ?>">
+                            </fieldset>
+                        </form>
+                        <?php if (has_menu_items()) : ?> 
+                            <ul class="menu__left menu__list">
+                                <?php while(menu_items()): ?> 
+                                    <li class="menu__item <?php echo (menu_active() ? 'menu__item--active"' : ''); ?>">
+                                        <a class="menu__link" href="<?php echo menu_url(); ?>" title="<?php echo menu_title(); ?>"><?php echo menu_name(); ?></a>
+                                    </li>
+                                <?php endwhile; ?>
+                            </ul>
+                            <ul class="menu__center menu__list">
+                                <?php while(categories()) : ?>
+                                    <li class="menu__item">
+                                        <a class="menu__link" href="<?php echo category_url(); ?>" title="<?php echo category_description(); ?>">
+                                            <?php echo category_title(); ?> (<?php echo category_count(); ?>)
+                                        </a>
+                                    </li>
+                                <?php endwhile; ?>
+                            </ul>
+                            <ul class="menu__right menu__list">
+                                <li class="menu__item"><a class="menu__link beard" href="http://www.bhalash.com" title="Real Men Wear Beards">Real Men Wear Beards</a></li>
+                                <li class="menu__item"><a class="menu__link facebook" href="http://www.facebook.com" title="Facebook">Facebook</a></li>
+                                <li class="menu__item"><a class="menu__link labs" href="http://www.091labs.com" title="091 Labs">091 Labs</a></li>
+                                <li class="menu__item"><a class="menu__link rss" href="<?php rss_url(); ?>" title="RSS Feed">RSS Feed</a></li>
+                            </ul>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </nav>
