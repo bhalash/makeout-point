95add60f1c2c1257f8efbb34134170203b0ab34d
Mark Grealish <mark@bhalash.com>
2015-11-29 17:07:53 +0000

    Refactored 404 CSS classes into BEM style.

--- 404.php
+++ 404.php
@@ -1,6 +1,8 @@
 <?php theme_include('header'); ?>
-    <div class="fail">
-        <h4>404: Page not found</h4>
-        <p>The eyes of my ancestors are cast down in shame!</p>
-    </div>
+
+<div class="fail">
+    <h4 class="faill__type">404: Page not found</h4>
+    <p class="fail__message">The eyes of my ancestors are cast down in shame!</p>
+</div>
+
 <?php theme_include('footer'); ?>
