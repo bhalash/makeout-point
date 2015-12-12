eda9bca503b77d39c0b3bcb65993e3f995af3537
Mark Grealish <mark@bhalash.com>
2015-12-12 16:11:25 +0000

    Added boilerplate comments to all functions.

--- functions.php
+++ functions.php
@@ -1,21 +1,44 @@
 <?php
 
-function twitter_account() {
-    return site_meta('twitter', 'bhalash');
+/**
+ * Twiter URL
+ * -----------------------------------------------------------------------------
+ * @param   string      $default        Default twitter account to use.
+ */
+
+function meta_twitter_href($default = 'bhalash') {
+    printf('https://twitter.com/%s', site_meta('twitter', $default));
 }
 
-function twitter_url() {
-    return 'https://twitter.com/' . twitter_account();
-}
+/**
+ * Count Comments
+ * -----------------------------------------------------------------------------
+ * Anchor returns the /total/ count of comments, not the total count for the
+ * post.
+ *
+ * @return  string                      Total number of comments.
+ * @link https://github.com/anchorcms/anchor-cms/issues/357
+ */
 
-// Anchor returns the /total/ count of comments, not the total count for the post.
-// See https://github.com/anchorcms/anchor-cms/issues/357
-function mp_article_total_comments() {
+function total_comments() {
     return Comment::where('post', '=', article_id())->count();
 }
 
-function partial($slug) {
-    include(sprintf('%s/partials/_%s.php', __DIR__, $slug));
+/**
+ * Fetch Partial
+ * -----------------------------------------------------------------------------
+ * Output post header information (category and date).
+ *
+ * @param   string      $partial        Partial file.
+ * @param   string      $variant        File variant.
+ */
+
+function partial($partial, $variant = null) {
+    if ($variant) {
+        $variant = '-' . $variant;
+    }
+
+    include(sprintf('%s/partials/_%s%s.php', __DIR__, $partial, $variant));
 }
 
 ?>
