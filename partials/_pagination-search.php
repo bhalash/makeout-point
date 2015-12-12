bf353e707660e2e06d5f2354665532dc4ed1b19d
Mark Grealish <mark@bhalash.com>
2015-12-12 17:08:58 +0000

    Added pagination for search results.

---
+++ partials/_pagination-search.php
@@ -0,0 +1,4 @@
+<nav class="pagination pagination--search">
+    <span class="pagination__previous"><?php echo search_prev(); ?></span>
+    <span class="pagination__next"><?php echo search_next(); ?></span>
+</nav>
