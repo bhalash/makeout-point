82dc026e19fbf00bd0962291663afaafe9ffd770
Mark Grealish <mark@bhalash.com>
2015-12-12 16:45:18 +0000

    Reduced templates to mimimum to function.

--- article.php
+++ article.php
@@ -1,27 +1,13 @@
-<?php partial('header'); ?>
+<?php
 
-<article id="article-<?php echo article_id(); ?>">
-    <h3 class="article-title"><a href="<?php echo article_url(); ?>"><?php echo article_title();?></a></h3>
-    <p class="article-info">
-        by <a href="mailto:<?php echo user_authed_email(); ?>"><?php echo article_author(); ?></a>
-         on <time datetime="<?php echo article_date(); ?>"><?php echo article_date(); ?></time>
-         in <a href="<?php echo category_url(); ?>"><?php echo category_title(); ?></a>
-        <?php if (comments_open()) : ?>
-            <span>
-                <a class="comments-button" href="#comment"><?php echo mp_article_total_comments(); ?> 
-                <?php if (mp_article_total_comments() != 1) : ?>
-                    comments</a>
-                <?php else : ?>
-                    comment</a>
-                <?php endif; ?>
-            </span>
-        <?php endif; ?>
-     </p>
-    <?php echo article_markdown(); ?>
-</article>
+partial('header');
+partial('article');
 
-<?php if (comments_open()) {
+
+if (comments_open()) {
     partial('comments');
-} ?>
+}
 
-<?php theme_include('footer'); ?>
+partial('footer');
+
+?>
--- page.php
+++ page.php
@@ -1,11 +1,7 @@
-<?php partial('header'); ?>
+<?php
 
-<article class="article article--page"id="article-<?php echo page_id(); ?>">
-    <h3 class="article__title"><a href="<?php echo page_url(); ?>"><?php echo page_title();?></a></h3>
-    <p class="article__info">&nbsp;</p>
-    <div class="article__content">
-        <?php echo page_content(); ?>
-    </div>
-</article>
+partial('header');
+partial('article');
+partial('footer');
 
-<?php partial('footer'); ?>
+?>
