48088c548752fb9bd50d030d00dd6ebf00dc4075
Mark Grealish <mark@bhalash.com>
2015-12-13 12:10:17 +0000

    Expanded functions.

--- functions.php
+++ functions.php
@@ -1,25 +1,6 @@
 <?php
 
 /**
- * Twiter URL
- * -----------------------------------------------------------------------------
- * @param   string      $default        Default twitter account to use.
- */
-
-function meta_twitter_href($default = 'bhalash') {
-    printf('https://twitter.com/%s', site_meta('twitter', $default));
-}
-
-/**
- * Comment Count Words
- * -----------------------------------------------------------------------------
- */
-
-function comment_count() {
-    printf(total_comments . ' ' . total_comments() != 0 ? 'comments' : 'comment');
-}
-
-/**
  * Count Comments
  * -----------------------------------------------------------------------------
  * Anchor returns the /total/ count of comments, not the total count for the
@@ -29,8 +10,59 @@ function comment_count() {
  * @link https://github.com/anchorcms/anchor-cms/issues/357
  */
 
-function total_comments() {
-    return Comment::where('post', '=', article_id())->count();
+function article_comment_count($article_id = null) {
+    $article_id = $article_id ?: article_id();
+    return Comment::where('post', '=', $article_id)->count();
+}
+
+/**
+ * Twiter URL
+ * -----------------------------------------------------------------------------
+ * @param   string      $fallback       Default twitter account to use.
+ * @return  string                      Twitter profile URL.
+ */
+
+function site_meta_twitter($fallback = 'bhalash') {
+    return sprintf('https://twitter.com/%s', site_meta('twitter', $fallback));
+}
+
+/**
+ * Pluralize Words
+ * -----------------------------------------------------------------------------
+ * This is a Terrible, Horrible, No Good, Very Bad pluralization solution.
+ *
+ * @param   string      $word           Word to pluralize.
+ * @param   int         $count          Quantity to pluralize against.
+ * @return  string      $word           Pluralized word.
+ */
+
+function pluralize($word, $count) {
+    if (abs($count) !== 1) {
+        switch ($word) {
+            case preg_match('/o$/', $word):
+                $word .= 's'; break;
+            case preg_match('/.+(s|x|z|ch|sh|)$/', $word):
+                $word .= 'es'; break;
+            default:
+                $word .= 's';
+        }
+    }
+
+    return $word;
+}
+
+/**
+ * Wrap Pluralized Word
+ * -----------------------------------------------------------------------------
+ * This is a Terrible, Horrible, No Good, Very Bad pluralization solution.
+ *
+ * @param   string      $word           Word to pluralize.
+ * @param   int         $count          Quantity to pluralize against.
+ * @return                              Pluralized word preceded by count.
+ */
+
+function pluralize_wrap($word, $count) {
+    return sprintf('%s %s', $count, pluralize($word, $count));
 }
 
 /**
@@ -51,4 +83,3 @@ function partial($partial, $variant = null) {
 }
 
 ?>
- * @return
