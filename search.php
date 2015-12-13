6767404917b9d36615bfb4a04393ef5370c4d2d1
Mark Grealish <mark@bhalash.com>
2015-12-13 12:10:41 +0000

    Further DRY work on views.

--- search.php
+++ search.php
@@ -5,26 +5,16 @@
 
         <div class="fail">
             <h4>Results for '<?php echo search_term(); ?>'</h4>
-            <?php if(total_search_results() == 1) : ?>
-                <p><?php echo total_search_results(); ?> total result.</p>
-            <?php else: ?>
-                <p><?php echo total_search_results(); ?> total results.</p>
-            <?php endif; ?>
+            <p><?php pluralize_wrap('result', total_search_results()); ?></p>
         </div>
 
         <?php partial('article'); ?>
 
-        <?php if(has_pagination()): ?>
-            <nav class="pagination">
-                <div class="wrap">
-                    <?php echo search_prev(); ?>
-                    <?php echo search_next(); ?>
-                </div>
-            </nav>
-        <?php endif; ?>
+        <?php if (has_pagination()) {
+            partial('pagination', 'search');
+        } ?>
+
     <?php endwhile; ?>
-<?php else:
-    include('404.php');
-endif; ?>
+<?php endif; ?>
 
 <?php partial('footer'); ?>
